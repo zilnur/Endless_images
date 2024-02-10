0d18229ef707a791b9edc164e595aed5ec679446
zilnur <86298777+zilnur@users.noreply.github.com>
2024-02-10 20:26:25 +0300

    Initial Commit

---
+++ Endless images/Endless_imagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Endless_imagesApp.swift
+//  Endless images
+//
+//  Created by Ильнур Закиров on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Endless_imagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
